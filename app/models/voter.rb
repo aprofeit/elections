deb379edc84a0c05dd4ca01e2b40780dce5584f5
Alexander Profeit <alex@profeit.com>
2020-03-27 18:40:53 -0400

    add link generation

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,6 +2,14 @@ class Voter < ApplicationRecord
   before_create :create_token
   has_many :votes
 
+  def invite_link
+    if Rails.env.production?
+      "https://elections.profeit.com/votes/new?token=#{token}"
+    else
+      "http://localhost:3000/votes/new?token=#{token}"
+    end
+  end
+
   private
 
   def create_token
